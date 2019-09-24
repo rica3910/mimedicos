e2374ce6ee3ed39d697a37f78fc1a2fc553ca8e7
Ricardo Salvador Luna Lozoya <ricardoluna@MacBook-Air-de-Ricardo.local>
2019-09-23 22:11:52 -0600

    ok

--- src/app/consultas/alta-receta/alta-receta.component.ts
+++ src/app/consultas/alta-receta/alta-receta.component.ts
@@ -20,8 +20,9 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, Validators } from '@angular/forms';
 import { AbstractControl } from '@angular/forms';
 import { FormulariosService } from './../../formularios.service';
-import { Subject } from 'rxjs';
+import { Subject, fromEvent } from 'rxjs';
 import { ConsultasService } from './../../consultas.service';
+import { switchAll, debounceTime, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-alta-receta',
@@ -44,6 +45,12 @@ export class AltaRecetaComponent implements OnInit {
   referenciasControl: AbstractControl;
   //Variable que almacena el control de referencias del formulario.
   @ViewChild('referenciasHTML') referenciasHTML: ElementRef;
+  //Cuadro de texto de búsqueda.
+  @ViewChild('buscarInfoHTML') buscarInfoHTML: ElementRef;
+  //Almacena los medicamentos y sus indicaciones (se utiliza para filtrar).
+  medicamentos: JSON[] = [];
+  //Almacena los medicamentos previamente añadidos a la receta.
+  medicamentosOriginal: JSON[] = [];
 
   /*----------------------------------------------------------------------|
   |  NOMBRE: constructor.                                                 |
@@ -87,6 +94,31 @@ export class AltaRecetaComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    //Se obtiene el método de tecleado del elemento HTML de búsqueda.
+    fromEvent(this.buscarInfoHTML.nativeElement, 'keyup')
+      //Extrae el valor de la búsqueda.
+      .pipe(map((e: any) => e.target.value))
+      //Se realiza la búsqueda.
+      .pipe(map((query: string) => this.utilidadesService.filtrarDatos(query, this.medicamentosOriginal)))
+      //Se utiliza para obtener solo la búsqueda más reciente.
+      .pipe(switchAll())
+      //Se actualiza la información del arreglo.
+      .subscribe((resultados: JSON[]) => {
+        //Se actualiza la información en pantalla.        
+        this.medicamentos = resultados;
+      });
+
+    //Evento de cuando se pega con el mouse algun texto en la caja de texto.
+    fromEvent(this.buscarInfoHTML.nativeElement, 'paste')
+      //Extrae el texto del cuadro de texto.
+      .pipe(map((e: any) => e.target.value))
+      .pipe(debounceTime(50))
+      //Se subscribe al evento.
+      .subscribe((cadena: string) => {
+        //Genera un evento de teclazo para asegurar que se dispare el evento.
+        this.buscarInfoHTML.nativeElement.dispatchEvent(new Event('keyup'));
+      });
   }
 
   /*----------------------------------------------------------------------|
@@ -117,6 +149,60 @@ export class AltaRecetaComponent implements OnInit {
   infoConsulta(info) {
     this.verificarInfoConsulta = true;
     this.cargaInicialLista$.next(this.verificarInfoConsulta);
+  }
+
+  /*----------------------------------------------------------------------|
+  |  NOMBRE: limpiarCampoBusqueda.                                        |
+  |-----------------------------------------------------------------------|
+  |  DESCRIPCIÓN: Limpia el campo de búsqueda y restablece la info. orig. | 
+  |-----------------------------------------------------------------------|
+  |  AUTOR: Ricardo Luna.                                                 |
+  |-----------------------------------------------------------------------|
+  |  FECHA: 22/09/2019.                                                   |    
+  |----------------------------------------------------------------------*/
+  limpiarCampoBusqueda() {
+
+    //Si el campo tiene algo escrito se limpiará.
+    if (this.buscarInfoHTML.nativeElement.value.length > 0) {
+      //limpia el cuadro de texto.
+      this.buscarInfoHTML.nativeElement.value = "";
+      //Actualiza la información con la original.
+      this.medicamentos = this.medicamentosOriginal;
+    }
+    //Le da un focus al elemento de búsqueda.
+    this.buscarInfoHTML.nativeElement.focus();
+  }
+
+  /*----------------------------------------------------------------------|
+  |  NOMBRE: altaReceta.                                                  |
+  |-----------------------------------------------------------------------|
+  |  DESCRIPCIÓN: Método para dar de alta una receta.                     | 
+  |-----------------------------------------------------------------------|
+  |  AUTOR: Ricardo Luna.                                                 |
+  |-----------------------------------------------------------------------|
+  |  FECHA: 22/09/2019.                                                   |    
+  |----------------------------------------------------------------------*/
+  altaReceta() {
+
+    //Se pulsa el botón  de dar de alta consulta.
+    this.pulsarCrear = true;
+
+    /*Si los elementos del formulario estáticos requeridos no están llenos, 
+    se hace un focus para que se ingrese texto.*/
+    if (this.referenciasControl.invalid) {
+      this.referenciasHTML.nativeElement.focus();
+      return;
+    }
+
+    //Si no se agregó ningún medicamento.
+    if (this.medicamentos.length == 0) {
+      this.utilidadesService.alerta("Sin medicamentos", "Agregue por lo menos un medicamento a la receta.").subscribe();
+      return;
+    }
+
+    //Se abre el modal de espera.
+    this.esperarService.esperar();
+
   }
 
 }
